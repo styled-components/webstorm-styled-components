d42c83f48242ddff58fe125b8f96ff56d3c5201e
Maxim Kropotov <maxim.kropotov@jetbrains.com>
2019-04-06 02:40:38 +0300

    support css prop: completion, avoid highlighting as unknown attribute
    check 'styled-components' v.4 and 'babel-plugin-styled-components' present

---
+++ src/main/kotlin/com/intellij/StyledComponents/CssPropAttributeDescriptorProvider.kt
@@ -0,0 +1,75 @@
+package com.intellij.styledComponents
+
+import com.intellij.javascript.nodejs.util.NodePackage
+import com.intellij.javascript.nodejs.util.NodePackageDescriptor
+import com.intellij.lang.javascript.DialectDetector
+import com.intellij.lang.javascript.frameworks.react.ReactAttributeDescriptorImpl
+import com.intellij.lang.javascript.psi.ecma6.impl.JSLocalImplicitElementImpl
+import com.intellij.lang.javascript.psi.types.JSNamedTypeFactory
+import com.intellij.lang.javascript.psi.types.JSTypeContext
+import com.intellij.lang.javascript.psi.types.JSTypeSource
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ProjectRootManager
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.psi.util.CachedValueProvider
+import com.intellij.psi.util.CachedValuesManager
+import com.intellij.psi.xml.XmlTag
+import com.intellij.xml.XmlAttributeDescriptor
+import com.intellij.xml.XmlAttributeDescriptorsProvider
+
+private const val BABEL_PLUGIN_NAME = "babel-plugin-styled-components"
+
+private const val STYLED_COMPONENTS_PACKAGE_NAME = "styled-components"
+
+private val JS_STRING_TYPE = JSNamedTypeFactory.createType("string", JSTypeSource.EMPTY, JSTypeContext.INSTANCE)
+
+class CssPropAttributeDescriptorProvider : XmlAttributeDescriptorsProvider {
+    override fun getAttributeDescriptors(tag: XmlTag?): Array<XmlAttributeDescriptor> {
+        if (tag != null && isCssPropSupported(tag)) {
+            return arrayOf(createCssPropertyDescriptor(tag))
+        }
+        return emptyArray()
+    }
+
+    override fun getAttributeDescriptor(name: String?, tag: XmlTag?): XmlAttributeDescriptor? {
+        if (tag != null && isCssPropSupported(tag) && name.equals("css")) {
+            return createCssPropertyDescriptor(tag)
+        }
+        return null
+    }
+
+    private fun createCssPropertyDescriptor(tag: XmlTag): XmlAttributeDescriptor {
+        val implicit = JSLocalImplicitElementImpl("css", JS_STRING_TYPE, tag, null)
+        return ReactAttributeDescriptorImpl.create("css", implicit, JS_STRING_TYPE, true)
+    }
+
+    private fun isCssPropSupported(tag: XmlTag): Boolean {
+        if (!DialectDetector.isJSX(tag)) {
+            return false
+        }
+        val containingFile = tag.containingFile?.originalFile
+        if (containingFile == null || containingFile.virtualFile == null) {
+            return false
+        }
+        if (ApplicationManager.getApplication().isUnitTestMode) {
+            return true
+        }
+        val virtualFile = containingFile.virtualFile
+        val project = containingFile.project
+        return CachedValuesManager.getManager(project).getCachedValue(containingFile) {
+            val styledComponentsPackage = getNodePackage(STYLED_COMPONENTS_PACKAGE_NAME, project, virtualFile)
+            val babelPluginPackage = getNodePackage(BABEL_PLUGIN_NAME, project, virtualFile)
+            val hasCssProp = styledComponentsPackage?.version?.isGreaterOrEqualThan(4, 0, 0) == true
+                    && babelPluginPackage != null
+            CachedValueProvider.Result(hasCssProp, VirtualFileManager.VFS_STRUCTURE_MODIFICATIONS,
+                    ProjectRootManager.getInstance(project))
+        }
+    }
+
+    private fun getNodePackage(packageName: String, project: Project, virtualFile: VirtualFile): NodePackage? {
+        val nodePackage = NodePackageDescriptor(packageName).findFirstDirectDependencyPackage(project, null, virtualFile)
+        return if (nodePackage.isValid) nodePackage else null
+    }
+}
--- src/test/CompletionTest.kt
+++ src/test/CompletionTest.kt
@@ -1,3 +1,8 @@
+import com.intellij.application.options.CodeStyle
+import com.intellij.codeInsight.lookup.Lookup
+import com.intellij.lang.javascript.formatter.JSCodeStyleSettings
+import com.intellij.psi.codeStyle.CodeStyleSettings
+import com.intellij.testFramework.UsefulTestCase
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase
 
 class CompletionTest : LightPlatformCodeInsightFixtureTestCase() {
@@ -14,5 +19,30 @@ class CompletionTest : LightPlatformCodeInsightFixtureTestCase() {
                         "`")
         val lookupElements = myFixture.completeBasic().map { it.lookupString }
         assertContainsElements(lookupElements, "red", "blue")
+    }
+
+    fun testCssPropInJsx() {
+        myFixture.configureByText("test.jsx", "<div <caret>/>")
+        val lookupElements = myFixture.completeBasic().map { it.lookupString }
+        assertContainsElements(lookupElements, "css")
+    }
+
+    fun testCompleteCssPropWithQuotesForJSXAttributeSetting() {
+        CodeStyle.doWithTemporarySettings(myFixture.project, CodeStyleSettings()) {
+            val jsCodeStyleSettings = CodeStyle.getSettings(myFixture.project).getCustomSettings(JSCodeStyleSettings::class.java)
+            jsCodeStyleSettings.JSX_ATTRIBUTE_VALUE = JSCodeStyleSettings.JSXAttributeValuePresentation.TYPE_BASED
+            myFixture.configureByText("test.jsx", "<div <caret>/>")
+            val cssItem = myFixture.completeBasic().find { it.lookupString == "css" }
+            assertNotNull("expected 'css' item", cssItem)
+            myFixture.lookup.currentItem = cssItem
+            myFixture.finishLookup(Lookup.NORMAL_SELECT_CHAR)
+            myFixture.checkResult("<div css=\"\"/>")
+        }
+    }
+
+    fun testNoCssPropInHtml() {
+        myFixture.configureByText("test.html", "<div <caret>/>")
+        val lookupElements = myFixture.completeBasic().map { it.lookupString }
+        UsefulTestCase.assertDoesntContain(lookupElements, "css")
     }
 }
