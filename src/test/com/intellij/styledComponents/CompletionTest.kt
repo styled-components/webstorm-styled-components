c923658beb48c95d0eaee466862075a7f9a575df
vladimir.panimaskin <Vladimir.Panimaskin@jetbrains.com>
2021-06-15 19:43:27 +0300

    WEB-48767: styled-components className completion tests
    (cherry picked from commit ed8572798c5d38254ed1e379a1da42afb5c3f095)
    
    IJ-CR-10148
    
    GitOrigin-RevId: 39b384041a1a6b199be5c9af949dcfa0a052a5ef

--- src/test/com/intellij/styledComponents/CompletionTest.kt
+++ src/test/com/intellij/styledComponents/CompletionTest.kt
@@ -8,6 +8,8 @@ import com.intellij.testFramework.UsefulTestCase
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 
 class CompletionTest : BasePlatformTestCase() {
+  override fun getBasePath(): String = "plugins/styled-components/testData/completion"
+
   fun testCompletionAfterInterpolationExpressionInParentheses() {
     myFixture.configureByText("dummy.es6",
                               "const HeroImage = styled(Box)`\n" +
@@ -31,14 +33,14 @@ class CompletionTest : BasePlatformTestCase() {
 
   fun testCompleteCssPropWithQuotesForJSXAttributeSetting() {
     CodeStyle.doWithTemporarySettings(myFixture.project, CodeStyleSettingsManager.createTestSettings(null), Runnable {
-        val jsCodeStyleSettings = CodeStyle.getSettings(myFixture.project).getCustomSettings(JSCodeStyleSettings::class.java)
-        jsCodeStyleSettings.JSX_ATTRIBUTE_VALUE = JSCodeStyleSettings.JSXAttributeValuePresentation.TYPE_BASED
-        myFixture.configureByText("test.jsx", "<div <caret>/>")
-        val cssItem = myFixture.completeBasic().find { it.lookupString == "css" }
-        assertNotNull("expected 'css' item", cssItem)
-        myFixture.lookup.currentItem = cssItem
-        myFixture.finishLookup(Lookup.NORMAL_SELECT_CHAR)
-        myFixture.checkResult("<div css=\"\"/>")
+      val jsCodeStyleSettings = CodeStyle.getSettings(myFixture.project).getCustomSettings(JSCodeStyleSettings::class.java)
+      jsCodeStyleSettings.JSX_ATTRIBUTE_VALUE = JSCodeStyleSettings.JSXAttributeValuePresentation.TYPE_BASED
+      myFixture.configureByText("test.jsx", "<div <caret>/>")
+      val cssItem = myFixture.completeBasic().find { it.lookupString == "css" }
+      assertNotNull("expected 'css' item", cssItem)
+      myFixture.lookup.currentItem = cssItem
+      myFixture.finishLookup(Lookup.NORMAL_SELECT_CHAR)
+      myFixture.checkResult("<div css=\"\"/>")
     })
   }
 
@@ -46,5 +48,27 @@ class CompletionTest : BasePlatformTestCase() {
     myFixture.configureByText("test.html", "<div <caret>/>")
     val lookupElements = myFixture.completeBasic().map { it.lookupString }
     UsefulTestCase.assertDoesntContain(lookupElements, "css")
+  }
+
+  fun testAttrClassNames() {
+    myFixture.copyFileToProject("styledComponentsStylesheet.css")
+    doTest(arrayListOf("layout", "layout-wide", "layout-narrow"))
+  }
+
+  fun testAttrClassNamesNestedExpression() {
+    myFixture.copyFileToProject("styledComponentsStylesheet.css")
+    doTest(arrayListOf("layout-wide", "layout-narrow"))
+  }
+
+  fun testAttrClassNamesFunc() {
+    myFixture.copyFileToProject("styledComponentsStylesheet.css")
+    doTest(arrayListOf("col-xs", "col-sm"))
+  }
+
+  private fun doTest(expected: Collection<String>, ext: String = ".js") {
+    val testName = "${getTestName(true)}$ext"
+    myFixture.configureByFile(testName)
+    val lookupElements = myFixture.completeBasic()?.map { it.lookupString } ?: emptyList()
+    assertContainsElements(lookupElements, expected)
   }
 }
---
+++ testData/completion/attrClassNamesFunc.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const Component = styled.input.attrs(props => ({
+  className: `col<caret>`
+}))``;
