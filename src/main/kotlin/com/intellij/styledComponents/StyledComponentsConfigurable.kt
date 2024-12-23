602bfddd35417a52675f45a20e76d6781eee6710
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-12-23 09:25:18 +0100

    do not export deps blindly (part 5)
    GitOrigin-RevId: 48daba2ea218703ecd0f60b5b12cc813c8802338

--- src/main/kotlin/com/intellij/styledComponents/StyledComponentsConfigurable.kt
+++ src/main/kotlin/com/intellij/styledComponents/StyledComponentsConfigurable.kt
@@ -25,7 +25,7 @@ import javax.swing.JPanel
 import javax.swing.JTable
 import javax.swing.table.AbstractTableModel
 
-class StyledComponentsConfigurable(private val project: Project) : SearchableConfigurable {
+private class StyledComponentsConfigurable(private val project: Project) : SearchableConfigurable {
   private val myConfiguration = CustomInjectionsConfiguration.instance(project)
   private val tagsModel = TagsModel()
   private val disposable = Disposer.newDisposable()
